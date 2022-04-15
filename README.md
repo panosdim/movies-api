5c75725ea279c3557ff6a3386093f8f6569f63ff
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2022-04-15 11:46:29 +0300

    Add installation script.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# movies-backend
-RESTful API backend in Go lang to support movies tracking site.
+# Movies Watch List REST API
+
+RESTful API backend in Go lang to support [movies web site](https://github.com/panosdim/movies-web-app).
+
+## REST endpoints
+
+Check [Postman documentation](https://documenter.getpostman.com/view/4800685/SVfTPnRY)
+
+## Libraries Used
+
+-   [Gin Web Framework](https://github.com/gin-gonic/gin)
+-   [GORM](https://gorm.io/)
+-   [Go Lang TMDb](https://github.com/cyruzin/golang-tmdb)
+-   [Go Dot Env](https://github.com/joho/godotenv)
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+if [ "$(id -u)" != "0" ]; then
+    exec sudo bash "$0" "$@"
+fi
+
+is_service_exists() {
+    x="$1"
+    if systemctl status "${x}" 2>/dev/null | grep -Fq "Active:"; then
+        return 0
+    else
+        return 1
+    fi
+    unset x
+}
+
+INSTALL_PATH=/opt/movies
+
+# Build software
+go build -o movies
+ret_code=$?
+if [ $ret_code != 0 ]; then
+    printf "Error: [%d] when building executable. Check that you have go tools installed." $ret_code
+    exit $ret_code
+fi
+
+# Check if needed files exist
+if [ -f .env ] && [ -f movies ] && [ -f movies.service ]; then
+    # Check if we upgrade or install for first time
+    if is_service_exists 'movies.service'; then
+        systemctl stop movies.service
+        cp movies $INSTALL_PATH
+        cp .env $INSTALL_PATH
+        systemctl start movies.service
+    else
+        mkdir -p $INSTALL_PATH
+        cp movies $INSTALL_PATH
+        cp .env $INSTALL_PATH
+        cp movies.service /usr/lib/systemd/system
+        systemctl start movies.service
+        systemctl enable movies.service
+    fi
+else
+    echo "Not all needed files found. Installation failed."
+    exit 1
+fi
